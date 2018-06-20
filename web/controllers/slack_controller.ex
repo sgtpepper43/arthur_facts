112924e69425fe6b1820cd584f325566cf072b38
Trevor Fenn <sgtpepper43@gmail.com>
2018-06-20 14:50:56 -0600

    Return passed text

--- web/controllers/slack_controller.ex
+++ web/controllers/slack_controller.ex
@@ -9,11 +9,21 @@ defmodule ArthurFacts.SlackController do
     resp(conn, 200, "You can't post Arthur facts in #{channel}!")
   end
 
+  def get_fact(conn, %{"text" => text} = params) when not is_nil(text) do
+    case String.trim(text) do
+      "" -> get_fact(conn, Map.drop("text"))
+      text -> send_response(text, conn, params)
+    end
+  end
+
   def get_fact(conn, params) do
-    fact =
-      params
-      |> Map.get("channel_name")
-      |> Fact.get()
+    params
+    |> Map.get("channel_name")
+    |> Fact.get()
+    |> send_response(conn, params)
+  end
+
+  defp send_response(fact, conn, params) do
     conn = resp(conn, 204, "")
     HTTPoison.post(params["response_url"], Poison.encode!(%{
       response_type: "in_channel",
