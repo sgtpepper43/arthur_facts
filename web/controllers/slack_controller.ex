38629b071fea895ff40ddd244d40efa3f019ec26
Trevor Fenn <sgtpepper43@gmail.com>
2018-06-22 14:05:15 -0600

    Only allow certain users to post whatever they want

--- web/controllers/slack_controller.ex
+++ web/controllers/slack_controller.ex
@@ -4,12 +4,14 @@ defmodule ArthurFacts.SlackController do
   alias ArthurFacts.Fact
 
   @forbidden_channels ~w(general)
+  @custom_text_users ~w(trevor james jbrand dennis.beatty korndog)
 
   def get_fact(conn, %{"channel_name" => channel}) when channel in @forbidden_channels do
     resp(conn, 200, "You can't post Arthur facts in #{channel}!")
   end
 
-  def get_fact(conn, %{"text" => text} = params) when not is_nil(text) do
+  def get_fact(conn, %{"text" => text, "user_name" => user_name} = params)
+      when not is_nil(text) and user_name in @custom_text_users do
     case String.trim(text) do
       "" -> get_fact(conn, Map.drop(params, ["text"]))
       text -> send_response(text, conn, params)
@@ -25,10 +27,15 @@ defmodule ArthurFacts.SlackController do
 
   defp send_response(fact, conn, params) do
     conn = resp(conn, 204, "")
-    HTTPoison.post(params["response_url"], Poison.encode!(%{
-      response_type: "in_channel",
-      text: fact
-    }), [{"Content-Type", "application/json"}])
+
+    HTTPoison.post(
+      params["response_url"],
+      Poison.encode!(%{
+        response_type: "in_channel",
+        text: fact
+      }),
+      [{"Content-Type", "application/json"}]
+    )
 
     conn
   end
